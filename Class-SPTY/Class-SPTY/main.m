dce164442ec041f788dcfb8ae9ca65880cf809c9
Adriano Soares <gsoares.adriano@gmail.com>
2015-02-13 15:45:30 -0200

    Initial Commit

---
+++ Class-SPTY/Class-SPTY/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Class-SPTY
+//
+//  Created by Adriano Soares on 13/02/15.
+//  Copyright (c) 2015 Adriano Soares. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
