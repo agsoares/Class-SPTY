bf84c3ca7bbdda29e2b757ab5ee14c2fee0d2cb1
Adriano Soares <gsoares.adriano@gmail.com>
2015-02-19 16:48:39 -0200

    end of the day commit

--- Class-SPTY/Class-SPTY/main.m
+++ Class-SPTY/Class-SPTY/main.m
@@ -12,16 +12,11 @@
 
 User *user;
 
-void login() {
-    
-
-}
-
 void createPlaylist(){
     char name[100];
     NSLog(@"Playlist name:");
     scanf("%s", name);
-    Playlist *playlist =[[Playlist alloc] initWithName:[[NSString alloc] initWithUTF8String:name]];
+    Playlist *playlist = [[Playlist alloc] initWithName:[[NSString alloc] initWithUTF8String:name]];
     [[playlist musics] addObject:@"Musica 1"];
     NSLog(@"%@", [playlist name]);
 }
@@ -29,8 +24,34 @@ void createPlaylist(){
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        int option;
         
+        char username[25];
+        char password[25];
+        
+        NSLog(@"Login");
+        NSLog(@"Username: ");
+        scanf("%s", username);
+        NSLog(@"Password: ");
+        scanf("%s", password);
+        
+        NSString *_username = [[NSString alloc] initWithUTF8String:username];
+        NSString *_password = [[NSString alloc] initWithUTF8String:password];
+        
+        NSArray *array = [[NSArray alloc] initWithContentsOfFile: @"Users.plist"];
+        for (int i = 0 ; i<[array count]; i++) {
+            NSDictionary *users = [[NSDictionary alloc] initWithDictionary:[array objectAtIndex:i]];
+            if([_username isEqualToString:[users valueForKey:@"screenname"]] && [_password isEqualToString:[users valueForKey:@"password"]]){
+                NSLog(@"Login Feito");
+                i = (int)[array count];
+            }
+            else{
+                NSLog(@"Fail");
+            }
+        }
+        
+        
+        int option;
+        system("clear");
         NSLog(@"1 - List Albuns");
         NSLog(@"2 - List Musics");
         NSLog(@"3 - Create Playlist");
